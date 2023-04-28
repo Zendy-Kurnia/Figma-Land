12f03083da35d5d6c1dfe5794589029a7feba23f
Kurnia <kurniawanviic54@gmail.com>
2023-04-29 02:02:59 +0700

    Add favicon and meta decription. Add features video. Add functionality to play button. Add box-shadow and transform hover effect on button.main

--- main.js
+++ main.js
@@ -58,7 +58,7 @@ $(function() {
                 document.body.appendChild(el2)
             }
         }
-        
+
         // Load maps iframe only if user scroll down to #contact
         if (scrollTop >= contactOffset) {
             if ($('.google_maps iframe').length == 0) {
@@ -83,6 +83,14 @@ $(function() {
         $('#mobile-menu').hasClass('show') ? $('.mobile-button').html('<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24"><path d="M18.3 5.71a.996.996 0 0 0-1.41 0L12 10.59L7.11 5.7A.996.996 0 1 0 5.7 7.11L10.59 12L5.7 16.89a.996.996 0 1 0 1.41 1.41L12 13.41l4.89 4.89a.996.996 0 1 0 1.41-1.41L13.41 12l4.89-4.89c.38-.38.38-1.02 0-1.4z"/></svg>') : $('.mobile-button').html('<svg viewBox="0 0 40 32" xmlns="http://www.w3.org/2000/svg"><path d="M6.66797 8H33.3346V10.6667H6.66797V8ZM13.3346 14.6667H33.3346V17.3333H13.3346V14.6667ZM21.668 21.3333H33.3346V24H21.668V21.3333Z"/></svg>')
     })
 
-    
+    // Play features video
+    var playBtns = $('.play') 
+    var s = playBtns.parents().find('video')[0].duration*1000
+    playBtns.click(function () {
+        playBtns.parents().find('video').trigger('play')
+        $(this).css({ display: 'none' })
+        playBtns.parents().find('video').attr('controls', '')
+    })
+
     // End of jQuery
 })
