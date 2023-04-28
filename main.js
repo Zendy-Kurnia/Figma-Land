62ed9921235e4c21a3bd9aceb10d52064c1625d5
Kurnia <kurniawanviic54@gmail.com>
2023-04-28 22:35:09 +0700

    Fix Header on small 360px device, set overflow x to hidden, fix logo size for mobile, set responsive size mobile-button menu with clamp()

--- main.js
+++ main.js
@@ -65,14 +65,16 @@ $(function() {
     // hide mobile menu if user click inside mobile menu
     // Click overlay to hide mobile-menu  
     // Disable scrolling if mobile menu is being shown
-    // Rotate mobile button 180deg if mobile menu is being shown
+    // Rotate mobile button 180deg if mobile menu is being shown and change the icon 
     
     $('.mobile-button, #mobile-menu, .overlay').click(function () {
         $('#mobile-menu').toggleClass('show')    
         $('.header-wrapper').toggleClass('bg-dark')
         $('#mobile-menu').hasClass('show') ? $('body').css({overflowY: 'hidden'}) : $('body').css({overflowY: 'auto'})
         $('#mobile-menu').hasClass('show') ? $('.mobile-button').css({transform: 'rotate(180deg)'}) : $('.mobile-button').css({transform: 'rotate(0)'}) 
+        $('#mobile-menu').hasClass('show') ? $('.mobile-button').html('<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24"><path d="M18.3 5.71a.996.996 0 0 0-1.41 0L12 10.59L7.11 5.7A.996.996 0 1 0 5.7 7.11L10.59 12L5.7 16.89a.996.996 0 1 0 1.41 1.41L12 13.41l4.89 4.89a.996.996 0 1 0 1.41-1.41L13.41 12l4.89-4.89c.38-.38.38-1.02 0-1.4z"/></svg>') : $('.mobile-button').html('<svg viewBox="0 0 40 32" xmlns="http://www.w3.org/2000/svg"><path d="M6.66797 8H33.3346V10.6667H6.66797V8ZM13.3346 14.6667H33.3346V17.3333H13.3346V14.6667ZM21.668 21.3333H33.3346V24H21.668V21.3333Z"/></svg>')
     })
 
+    
     // End of jQuery
 })
